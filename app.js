a432898c20c60f9e4f93bc9a0b4c7aae5ef9741d
Michael Sebring <michaelsebring@Michaels-MacBook-Pro.local>
2018-07-09 21:34:21 -0500

    Updated to remove and add list items

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
-const input = document.querySelector('input.description');
-const p = document.querySelector('p.description');
-const button = document.querySelector('button.description');
+const descriptionInput = document.querySelector('input.description');
+const descriptionP = document.querySelector('p.description');
+const descriptionButton = document.querySelector('button.description');
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
+const removeItemButton = document.querySelector('button.removeItemButton');
+
 
 toggleList.addEventListener('click', () => {
-  if (listDiv.style.display == 'none') {
+  if (listDiv.style.display === 'none') {
     toggleList.textContent = 'Hide list';
     listDiv.style.display = 'block';
   } else {
-    toggleList.textContent = 'Show list';                        
+    toggleList.textContent = 'Show list';
     listDiv.style.display = 'none';
-  }                         
+  }
 });
 
-button.addEventListener('click', () => {
-  p.innerHTML = input.value + ':';
+descriptionButton.addEventListener('click', () => {
+  descriptionP.innerHTML = descriptionInput.value + ':';
+  decsriptionInput.value = '';
 });
+
+
+addItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.createElement('li');
+  li.textContent = addItemInput.value;
+  ul.appendChild(li);
+  addItemInput.value = '';
+});
+
+removeItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.querySelector('li:last-child');
+  ul.removeChild(li);
+})
 
